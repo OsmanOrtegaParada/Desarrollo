f5bbf0f266557160f71929e69fa528184cb73019
OsmanOrtegaParada <33299443+OsmanOrtegaParada@users.noreply.github.com>
2017-12-09 15:37:55 -0500

    Modificación del algoritmo
    Se hna agregado los métodos para el bordado de la imagen

--- Proceso/CodigoMetodosImagenes.py
+++ Proceso/CodigoMetodosImagenes.py
@@ -34,3 +34,34 @@ for i in xrange(3):
 plt.show()
 
 
+#Se agrega metodo para el bordado de la imagen
+
+
+# Cargamos la imagen
+img=cv2.imread('modenas2.jpg',0)
+bgr_img = cv2.imread('modenas2.jpg')
+b,g,r = cv2.split(bgr_img)      
+rgb_img = cv2.merge([r,g,b])
+# Aplicar suavizado Gaussiano
+gauss = cv2.GaussianBlur(img, (1,1), 0)
+
+# Detectamos los bordes con Canny
+canny = cv2.Canny(gauss, 50, 220)
+ 
+
+# Buscamos los contornos
+(_, contornos,_) = cv2.findContours(canny.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+
+cv2.drawContours(img,contornos,-1,(0,0,255), 2)
+
+titulos = ['Imagen Original','Simbolos Monedas']
+imagenes = [rgb_img,canny]
+for i in xrange(2):
+    plt.subplot(1,2,i+1),plt.imshow(imagenes[i],'gray')
+    plt.title(titulos[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
+
+
+
