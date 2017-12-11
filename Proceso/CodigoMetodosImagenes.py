dfe4212d26f0dd6b6635411db787a559f21313da
OsmanOrtegaParada <os_parada@hotmail.com>
2017-12-10 21:43:19 -0500

    Se realizo una modificacion a la carpeta Proceso
    Se añadio una nueva imagen para realizar el proceso de contornos.
    Adicionalmente al algoritmo se le agrego el metodo para realizar dicos
    contornos

--- Proceso/CodigoMetodosImagenes.py
+++ Proceso/CodigoMetodosImagenes.py
@@ -64,4 +64,42 @@ for i in xrange(2):
 plt.show()
 
 
+#Se agrega metodo para observar el contorno de las imagenes
+
+
+bgr_img = cv2.imread('manzanas.jpg')
+b,g,r = cv2.split(bgr_img)       
+rgb_img = cv2.merge([r,g,b])     
+
+gray = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2GRAY)
+gauss = cv2.GaussianBlur(gray, (7, 7), 0)
+canny = cv2.Canny(gauss, 50, 255)
+
+t, dst = cv2.threshold(canny, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_TRIANGLE)
+
+(_, contours, _) = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+for c in contours:
+    area = cv2.contourArea(c)
+    if area > 100 and area < 10000:
+        cv2.drawContours(rgb_img,contours,-1,(255,0,0), 2)
+
+titulos = ['Contorno']
+imagenes = [rgb_img]
+for i in xrange(1):
+    plt.subplot(1,1,i+1),plt.imshow(imagenes[i],'gray')
+    plt.title(titulos[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
+"""
+cv2.imshow('contornos', src)
+cv2.imshow('umbral', gray)
+cv2.waitKey(0)"""
+
+
+
+
+
+
+
 
